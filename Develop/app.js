6e893504a1830fa31e6b948fb9ca44582b278994
Zach Corpuz <zachcorpuz@yahoo.com>
2020-06-11 00:06:10 -0700

    created conditional statement that will have user go through the specific role prompt that they have chosen. Push all answers to empty array so we can fs.writefile

--- Develop/app.js
+++ Develop/app.js
@@ -10,50 +10,48 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-function promptQuestions() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is the name of the employee?',
-            validate: function(answer) {
-                if (answer.length < 1) {
-                    return 'Please enter valid name';
-                }
-                return true;
+const promptQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the employee?',
+        validate: function(answer) {
+            if (answer.length < 1) {
+                return 'Please enter valid name';
             }
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'What is the employee\'s ID number?',
-            validate: function(answer) {
-                if (answer = NaN || answer.length < 1) {
-                    return 'Please enter valid ID number';
-                }
-                return true;
-            }
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is the employee\'s email address?',
-            validate: function(answer) {
-                if (answer.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
-                    return true;
-                } else {
-                    return 'Invalid email. Please enter valid email';
-                };
-            }
-        },
-        {
-            type: 'rawlist',
-            name: 'role',
-            message: 'What is the employee\'s role?',
-            choices: ['Manager', 'Engineer', 'Intern']
+            return true;
         }
-     ])
-};
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the employee\'s ID number?',
+        validate: function(answer) {
+            if (answer = NaN || answer.length < 1) {
+                return 'Please enter valid ID number';
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the employee\'s email address?',
+        validate: function(answer) {
+            if (answer.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+                return true;
+            } else {
+                return 'Invalid email. Please enter valid email';
+            };
+        }
+    },
+    {
+        type: 'rawlist',
+        name: 'role',
+        message: 'What is the employee\'s role?',
+        choices: ['Manager', 'Engineer', 'Intern']
+    }
+];
 
 const managerPrompt = {
     type: 'input',
@@ -79,7 +77,7 @@ const engineerPrompt = {
     }
 };
 
-const interPrompt = {
+const internPrompt = {
     type: 'input',
     name: 'school',
     message: 'What school does the intern attend?',
@@ -92,9 +90,47 @@ const interPrompt = {
 };
 
 
+const employees = [];
 
-
+// Utilize async function so that process happens synchronous and you get proper information
 async function init() {
-    const generalAnswers = await promptQuestions();
-    const { name, id, email, role} = employeeInput;
-}
+    const generalAnswers = await inquirer.prompt(promptQuestions);
+    // Utilize Object Destructuring here to unpack values from promptQuestions array
+    const { name, id, email, role} = generalAnswers;
+
+    // Conditional statement that generates specific question to user based on role answer
+    if(role === 'Manager') {
+        // Manager prompt if user selects role "Manager"
+        const managerAnswer = await inquirer.prompt(managerPrompt);
+        const officeNumber = managerAnswer.officeNumber;
+
+        // New Constructor for Manager
+        const newManager = new Manager(name, id, email, officeNumber);
+        // Add the new employee to the Employees array
+        employees.push(newManager);
+
+    } else if(role === 'Engineer') {
+        // Manager prompt if user selects role "Engineer"
+        const engineerAnswer = await inquirer.prompt(engineerPrompt);
+        const github = engineerAnswer.github;
+
+        // New Constructor for Engineer
+        const newEngineer = new Engineer(name, id, email, github);
+        // Add the new employee to the Employees array
+        employees.push(newEngineer);
+
+    }else if(role === 'Intern') {
+        // Manager prompt if user selects role "Intern"
+        const internAnswer = await inquirer.prompt(internPrompt);
+        const school = internAnswer.school;
+
+        // New Constructor for Inter
+        const newIntern = new Intern(name, id, email, school);
+        // Add the new employee to the Employees array
+        employees.push(newIntern);
+    }
+
+    console.log(employees);
+};
+
+init();
